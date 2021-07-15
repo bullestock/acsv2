6b6206dd166c41974c4cc9844f58337e1a3ca901
Torsten Martinsen <torsten@bullestock.net>
2021-07-15 16:24:14 +0200

    Process FL schema.

--- synch/flapi.py
+++ synch/flapi.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from django.contrib.auth.models import User
 import logging
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 def _get_fl_json():
     logger = logging.getLogger("django")
     logger.info("ForeningLet synch starting")
-    BASE_DIR = Path(__file__).resolve().parent.parent
     logger.info("base %s" % BASE_DIR)
     yml_dir = os.path.join(BASE_DIR, 'synch')
     with open(yml_dir + '/secret.yml', encoding='utf-8', errors='replace') as ymlfile:
@@ -31,13 +31,38 @@ def _get_fl_json():
       
 def update_fl():
     logger = logging.getLogger("django")
-    json = _get_fl_json()
-    if json is not None:
+    members = _get_fl_json()
+    if members is not None:
         try:
+            yml_dir = os.path.join(BASE_DIR, 'synch')
+            with open(yml_dir + '/secret.yml', encoding='utf-8', errors='replace') as ymlfile:
+                yml = ymlfile.read()
+            secret = yaml.safe_load(yml)
+            activity_ids = secret['activity_ids']
+            logger.info("Activity IDs: %s" % activity_ids)
+            active_members = []
+            updated_members = []
+            added_members = []
+            nologin_members = []
+            excluded_members = []
+            for m in members:
+                id = int(m["MemberId"])
+                number = m["MemberNumber"]
+                first_name = m["FirstName"]
+                last_name = m["LastName"]
+                login = m["MemberField6"]
+                name = first_name + " " + last_name
+                activities = m["Activities"]
+                activities = int(activities) if activities.isdecimal() else None # stupid sexy ForeningLet
+                #logger.info("Member {0} activities: {1}".format(name, activities))
+                if not activities in activity_ids:
+                    logger.info("Member {0} (ID {1}) has no activities".format(name, id))
+                    excluded_members.append(name)
+            logger.info("Processed %d members" % len(members))
             # new_forecast = Forecast()
             # new_forecast.temperatue = temp_in_celsius
             # new_forecast.save()
             # logger.info("saving...\n" + new_forecast)
-            logger.info("saving")
-        except:
+        except Exception as e:
+            logger.info("Exception: %s" % e)
             pass
