a0aa8c7cf9b3881b60de827f7b54dd9e2bd8b6c5
Torsten Martinsen <torsten@bullestock.net>
2021-07-15 19:53:30 +0200

    Add Member model.

--- acs/settings.py
+++ acs/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'logs.apps.LogsConfig',
     'machines.apps.MachinesConfig',
+    'members.apps.MembersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
