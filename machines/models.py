610fe0ea5a86f2a76d12d63a4187d915992a54f3
Torsten Martinsen <torsten@bullestock.net>
2021-07-18 00:24:35 +0200

    Fix apitoken size.

--- machines/models.py
+++ machines/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class Machine(models.Model):
     user = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
     name = models.CharField(max_length=40, unique=True)
-    apitoken = models.CharField(max_length=32)
+    apitoken = models.CharField(max_length=64)
