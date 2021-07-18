dc08c029d2a5c583643c271f3de5f2a5f86a0766
Torsten Martinsen <torsten@bullestock.net>
2021-07-19 00:12:37 +0200

    Read secret key from file.

--- acs/settings.py
+++ acs/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_km7k%237vn9_w=!nhszwj8$a51u@svbq%64_+@pjmew$!gaux'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
