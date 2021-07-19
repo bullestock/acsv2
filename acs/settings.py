9e3900d43298c257d7355f057f9367c2ac69c864
Torsten Martinsen <torsten@bullestock.net>
2021-07-19 18:15:47 +0200

    Turn off DEBUG.

--- acs/settings.py
+++ acs/settings.py
@@ -26,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 with open('secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'localhost', 'panopticon.hal9k.dk' ]
 
 
 # Application definition
