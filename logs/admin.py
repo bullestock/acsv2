68bf8f14310b10671227bc62fdeeae6cd5217aed
Torsten Martinsen <torsten@bullestock.net>
2021-07-20 13:46:17 +0200

    Add search.

--- logs/admin.py
+++ logs/admin.py
@@ -3,4 +3,5 @@ from logs.models import Log
 
 class LogAdmin(admin.ModelAdmin):
     list_display = ('stamp', 'user', 'message')
+    search_fields = ('message',)
 admin.site.register(Log, LogAdmin)
