e53df8a33f11fb426d4ccf42bc436ba25a3428ab
Torsten Martinsen <torsten@bullestock.net>
2021-08-17 18:27:34 +0200

    Add machine to log view.

--- backend/logs/admin.py
+++ backend/logs/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from logs.models import Log
 
 class LogAdmin(admin.ModelAdmin):
-    list_display = ('stamp', 'user_id', 'message')
+    list_display = ('stamp', 'machine', 'user_id', 'message')
     search_fields = ('message',)
 admin.site.register(Log, LogAdmin)
