6373a604c60180471a3c7458b688347b748b82f9
Torsten Martinsen <torsten@bullestock.net>
2021-08-18 19:17:01 +0200

    Disable Add.

--- backend/unknown_cards/admin.py
+++ backend/unknown_cards/admin.py
@@ -3,4 +3,8 @@ from unknown_cards.models import UnknownCard
 
 class UnknownCardAdmin(admin.ModelAdmin):
     list_display = ('stamp', 'card_id')
+
+    def has_add_permission(self, request):
+        return False
+
 admin.site.register(UnknownCard, UnknownCardAdmin)
