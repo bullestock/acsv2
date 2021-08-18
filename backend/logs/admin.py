701ac28b0864e41e700e40b0c7b8c34a32eda387
Torsten Martinsen <torsten@bullestock.net>
2021-08-18 19:17:26 +0200

    Disable Add.

--- backend/logs/admin.py
+++ backend/logs/admin.py
@@ -4,4 +4,8 @@ from logs.models import Log
 class LogAdmin(admin.ModelAdmin):
     list_display = ('stamp', 'machine', 'user_id', 'message')
     search_fields = ('message',)
+
+    def has_add_permission(self, request):
+        return False
+
 admin.site.register(Log, LogAdmin)
