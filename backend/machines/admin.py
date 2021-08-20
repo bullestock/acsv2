e987e38e537d1dcb941a3a233cddbdcec1b9a735
Torsten Martinsen <torsten@bullestock.net>
2021-08-20 15:43:09 +0200

    Hide apitoken.

--- backend/machines/admin.py
+++ backend/machines/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from machines.models import Machine
 
 class MachineAdmin(admin.ModelAdmin):
-    list_display = ('name', 'apitoken')
+    list_display = ('name')
+
 admin.site.register(Machine, MachineAdmin)
