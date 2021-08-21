d755f2735f4c6db27f9117087293563677656f9d
Torsten Martinsen <torsten@bullestock.net>
2021-08-21 22:37:29 +0200

    Rename, update.

--- backend/synch/cards.py
+++ backend/synch/cards.py
@@ -6,9 +6,14 @@ import traceback
 import csv
 import os
 
+'''
+from synch import cards
+cards.import_cards()
+'''
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-def copy_users():
+def import_cards():
     logger = logging.getLogger("django")
     logger.info("Card synch starting")
     csv_path = os.path.join(BASE_DIR, 'users.csv')
@@ -16,11 +21,14 @@ def copy_users():
     with open(csv_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            fl_id = int(row[0]) if row[0].isdecimal() else None
+            if len(row) == 0:
+                continue
+            fl_id = row[0].strip()
+            fl_id = int(fl_id) if fl_id.isdecimal() else None
             if not fl_id:
                 continue
-            card_id = row[1]
-            if card_id == '\\N':
+            card_id = row[1].strip()
+            if len(card_id) == 0:
                 continue
             #logger.info("%d %s" % (fl_id, card_id))
             try:
