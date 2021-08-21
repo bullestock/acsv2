8de39b8b635f158bc9230c7ebe1ea5d29c2a9569
Torsten Martinsen <torsten@bullestock.net>
2021-08-21 16:02:38 +0200

    Add comma.

--- backend/machines/admin.py
+++ backend/machines/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from machines.models import Machine
 
 class MachineAdmin(admin.ModelAdmin):
-    list_display = ('name')
+    list_display = ('name',)
 
 admin.site.register(Machine, MachineAdmin)
