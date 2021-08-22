1a8bb8a9eddbc05ac1ebbd3cbef632f2e86377bf
Torsten Martinsen <torsten@bullestock.net>
2021-08-22 11:04:46 +0200

    Hide 'Important dates'; restrict superuser access.

--- backend/members/admin.py
+++ backend/members/admin.py
@@ -4,19 +4,21 @@ from django import forms
 from .models import Member
 
 class CustomUserAdmin(UserAdmin):
-    fieldsets = (
-        *UserAdmin.fieldsets,
-        (
-            'Custom fields',
-            {
-                'fields': (
-                    'fl_id',
-                    'card_id',
-                    'machine'
-                ),
-            },
-        ),
-    )
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+
+        if request.user.is_superuser:
+            perm_fields = ('is_active', 'is_staff', 'is_superuser',
+                           'groups', 'user_permissions')
+        else:
+            perm_fields = ('is_active', 'is_staff')
+
+        return [ (None, {'fields': ('username', 'password')}),
+                 ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+                 ('Permissions', {'fields': perm_fields}),
+                 ('Custom fields', {'fields': ('fl_id', 'card_id', 'machine')})
+                ]
 
     def get_form(self, request, obj=None, **kwargs):
         form = super(CustomUserAdmin, self).get_form(request, obj, **kwargs)
