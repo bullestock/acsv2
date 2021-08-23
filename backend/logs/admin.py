1b157a0f91a695b4c89f75b88f098a26317af33c
Torsten Martinsen <torsten@bullestock.net>
2021-08-23 21:57:23 +0200

    Remove unused /logs URL. Improve log search.

--- backend/logs/admin.py
+++ backend/logs/admin.py
@@ -3,7 +3,7 @@ from logs.models import Log
 
 class LogAdmin(admin.ModelAdmin):
     list_display = ('stamp', 'machine', 'user_id', 'message')
-    search_fields = ('message',)
+    search_fields = ('message', 'user_id__first_name', 'user_id__last_name', 'machine_id__name')
 
     def has_add_permission(self, request):
         return False
