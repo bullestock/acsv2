a4c658c6073affa51adc451ddf03ce9cc6274fbb
Torsten Martinsen <torsten@bullestock.net>
2021-08-28 09:38:38 +0200

    Prevent messing with the log.

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -8,3 +8,12 @@ class Log(models.Model):
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null = True)
     message = models.CharField(max_length=200)
     stamp = models.DateTimeField('timestamp', auto_now_add=True)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
