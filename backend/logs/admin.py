8639efd626047ba515e089b6c33e39cce79e6fab
Torsten Martinsen <torsten@bullestock.net>
2021-08-29 09:37:52 +0200

    Fix brain fart.

--- backend/logs/admin.py
+++ backend/logs/admin.py
@@ -8,4 +8,10 @@ class LogAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 admin.site.register(Log, LogAdmin)
