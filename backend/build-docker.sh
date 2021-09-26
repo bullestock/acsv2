fd2b0dc8eee37115c43c0c09db66f7c3266325c2
Torsten Martinsen <torsten@bullestock.net>
2021-09-26 10:49:45 +0200

    Add script for building image.

---
+++ backend/build-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+serial=`cat docker-serial`
+docker build -t bullestock/acsv2:$serial -t bullestock/acsv2:latest -f docker/Dockerfile --build-arg GIT_COMMIT=`git rev-parse HEAD` .
+docker push bullestock/acsv2:$serial
+docker push bullestock/acsv2:latest
+expr $serial + 1 > docker-serial
