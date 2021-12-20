07fc599bafa88da6fda7a3fe09bd1b5c596677cb
Hal9k <hal9k@oldacs.hal9k.dk>
2021-12-20 20:30:39 +0000

    Fix path.

--- ui/ui.sh
+++ ui/ui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#/home/hal9k/.rbenv/shims/ruby /home/hal9k/acs/ui/init.rb | tee /var/log/acs/ui.log
+/home/hal9k/.rbenv/shims/ruby /home/hal9k/acsv2/ui/init.rb | tee /var/log/acs/ui.log
 /home/hal9k/.rbenv/shims/ruby /home/hal9k/acsv2/ui/ui.rb | tee -a /var/log/acs/uiv2.log
