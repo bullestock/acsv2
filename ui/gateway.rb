d5f79d9d058a0ba572265bb5ee4459fb9a92b2e6
Torsten Martinsen <torsten@bullestock.net>
2021-12-20 22:29:24 +0100

    Add get_action().

--- ui/gateway.rb
+++ ui/gateway.rb
@@ -27,9 +27,33 @@ class Gateway
       puts "#{e.class} Failed to connect to gateway"
     end
   end
+
+  def get_action()
+    begin
+      uri = URI.parse("https://acsgateway.hal9k.dk/acsquery")
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/json"
+      request["Content-Type"] = "application/json"
+      request["Accept"] = "application/json"
+      body = { token: @token }
+      request.body = JSON.generate(body)
+      req_options = {
+        use_ssl: uri.scheme == "https",
+        read_timeout: 1,
+        open_timeout: 1
+      }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      puts "Response: #{response.body}"
+    rescue Exception => e
+      puts "#{e.class} Failed to connect to gateway"
+    end
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
   gw = Gateway.new
-  gw.set_status('Encoder position': 47, handle: 'raised', door: 'open')
+  #gw.set_status('Encoder position': 47, handle: 'raised', door: 'open')
+  gw.get_action()
 end 
