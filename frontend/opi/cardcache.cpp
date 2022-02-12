2d678b1f87b803c4cf2fd365c8ebf6371ffd6f2a
Torsten Martinsen <torsten@bullestock.net>
2022-02-12 22:09:51 +0100

    Implement REST call.

--- frontend/opi/cardcache.cpp
+++ frontend/opi/cardcache.cpp
@@ -1,9 +1,21 @@
 #include "cardcache.h"
 
+#include <iomanip>
+#include <iostream>
+
+#include <fmt/core.h>
+
+#include <restclient-cpp/restclient.h>
+#include <restclient-cpp/connection.h>
+
 constexpr util::duration MAX_CACHE_AGE = std::chrono::minutes(15);
+
+constexpr auto BASE_URL = "https://panopticon.hal9k.dk/api/v1";
+constexpr auto TOKEN = "topsecret";
 
 Card_cache::Card_cache()
 {
+    RestClient::init();
 }
 
 bool Card_cache::has_access(Card_cache::Card_id id)
@@ -15,5 +27,18 @@ bool Card_cache::has_access(Card_cache::Card_id id)
             return true;
         // Cache entry is outdated
     }
+
+    auto conn = std::make_unique<RestClient::Connection>(BASE_URL);
+    conn->SetTimeout(5);
+    conn->AppendHeader("Content-Type", "application/json");
+    conn->AppendHeader("Accept", "application/json");
+    util::json payload;
+    payload["api_token"] = TOKEN;
+    payload["card_id"] = fmt::format("{:10X}", id);
+    std::cout << "POST: " << payload << std::endl;
+    const auto resp = conn->post("/permissions", payload.dump());
+    std::cout << "resp.code: " << resp.code << std::endl;
+    std::cout << "resp.body: " << resp.body << std::endl;
+
     return false;
 }
--- frontend/opi/util.h
+++ frontend/opi/util.h
@@ -2,9 +2,13 @@
 
 #include <chrono>
 
+#include <nlohmann/json.hpp>
+
 namespace util
 {
     using duration = std::chrono::system_clock::duration;
     using time_point = std::chrono::system_clock::time_point;
     static inline time_point now() { return std::chrono::system_clock::now(); }
+
+    using json = nlohmann::json;
 }
