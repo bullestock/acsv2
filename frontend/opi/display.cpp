08ed86ca7dae57435a44d0127e560d2e9436fb17
Torsten Martinsen <torsten@bullestock.net>
2022-02-20 18:01:58 +0100

    Add queue.

--- frontend/opi/display.cpp
+++ frontend/opi/display.cpp
@@ -1,6 +1,56 @@
 #include "display.h"
 
 Display::Display(serialib& p)
-    : port(p)
+    : port(p),
+      thread(&Display::thread_body, this),
+      q(10)
 {
+}
+
+Display::~Display()
+{
+    thread.join();
+}
+
+void Display::clear()
+{
+    q.push({ Item::Type::Clear });
+}
+
+void Display::set_status(const std::string& text)
+{
+    Item item{ Item::Type::Set_status };
+    strncpy(item.s, text.c_str(), std::min(Item::MAX_SIZE, text.size()));
+    q.push(item);
+}
+
+void Display::show_message(const std::string& text,
+                           util::duration dur)
+{
+    Item item{ Item::Type::Show_message, "", dur };
+    strncpy(item.s, text.c_str(), std::min(Item::MAX_SIZE, text.size()));
+    q.push(item);
+}
+
+void Display::thread_body(Display* self)
+{
+    auto& port = self->port;
+    auto& q = self->q;
+    Item item;
+    while (1)
+        if (q.pop(item))
+            switch (item.type)
+            {
+            case Item::Type::Clear:
+                port.write("C\n");
+                break;
+
+            default:
+                std::cout << "Item type " << int(item.type) << " not handled\n";
+                break;
+            }
+        else
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
 }
--- frontend/opi/display.h
+++ frontend/opi/display.h
@@ -1,12 +1,44 @@
 #pragma once
 
 #include "serialib.h"
+#include "util.h"
+
+#include <thread>
+
+#include <boost/lockfree/queue.hpp>
 
 class Display
 {
 public:
     Display(serialib&);
 
+    ~Display();
+
+    /// Clear entire display
+    void clear();
+
+    /// Set status
+    void set_status(const std::string& text);
+
+    /// Show message for specified time
+    void show_message(const std::string& text,
+                      util::duration dur = std::chrono::seconds(5));                      
+
 private:
     serialib& port;
+    std::thread thread;
+    struct Item {
+        enum class Type {
+            Clear,
+            Set_status,
+            Show_message,
+        };
+        Type type = Type::Clear;
+        constexpr static size_t MAX_SIZE = 80;
+        char s[MAX_SIZE+1];
+        util::duration dur = std::chrono::seconds(0);
+    };
+    boost::lockfree::queue<Item> q;
+
+    static void thread_body(Display*);
 };
