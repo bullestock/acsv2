a66ca9c538d7125d50f53f7def42361886e27eb5
Torsten Martinsen <torsten@bullestock.net>
2022-03-01 21:20:32 +0100

    Fix line height for large text.

--- frontend/esp32/main/console.cpp
+++ frontend/esp32/main/console.cpp
@@ -140,69 +140,136 @@ static uint16_t colours[] = {
 static const int char_width_small = 8;
 static const int line_height_small = 18;
 static const int char_width_large = 16;
-static const int line_height_large = 25;
+static const int line_height_large = 30;
 static const int max_x_small = 39;
-static const int max_y_small = 12;
+static const int max_y_small = 11;
 static const int max_x_large = 19;
 static const int max_y_large = 8;
 
 // x,y,col,text
-bool text(bool large, const std::string s)
+bool text(bool large, bool erase, const std::string s)
 {
-    const auto first_sep = s.find(",");
-    if (first_sep == std::string::npos)
-    {
-        printf("Missing Y\n");
-        return false;
-    }
-    const auto second_sep = s.find(",", first_sep + 1);
-    if (second_sep == std::string::npos)
-    {
-        printf("Missing colour\n");
-        return false;
-    }
-    const auto third_sep = s.find(",", second_sep + 1);
-    if (third_sep == std::string::npos)
-    {
-        printf("Missing text\n");
-        return false;
-    }
+   std::vector<std::string> elems;
+   split(s, ",", elems);
+   if (elems.size() < 4)
+   {
+       printf("ERROR: Missing parameters\n");
+       return false;
+   }
+   auto text = elems[3];
+   for (size_t i = 4; i < elems.size(); ++i)
+   {
+       text += ",";
+       text += elems[i];
+   }
     const int max_x = large ? max_x_large : max_x_small;
     const int max_y = large ? max_y_large : max_y_small;
     int x, y, col;
-    if (!from_string(s.substr(0, first_sep), x) || x < 0 || x > max_x ||
-        !from_string(s.substr(first_sep + 1, second_sep - first_sep), y) || y < 0 || y > max_y)
+    if (elems[0].empty())
+        // No X specified, center
+        x = (CONFIG_HEIGHT - text.size())/2;
+    else if (!from_string(elems[0], x) || x < 0 || x > max_x)
     {
-        printf("Invalid coords\n");
+        printf("ERROR: Invalid X\n");
         return false;
     }
-    if (!from_string(s.substr(second_sep + 1, third_sep - second_sep), col) ||
+    if (!from_string(elems[1], y) || y < 0 || y > max_y)
+    {
+        printf("ERROR: Invalid Y\n");
+        return false;
+    }
+    if (!from_string(elems[2], col) ||
         col < 0 || col >= sizeof(colours)/sizeof(colours[0]))
     {
-        printf("Invalid colour\n");
+        printf("ERROR: Invalid colour\n");
         return false;
     }
-    const auto text = s.substr(third_sep + 1);
-    printf("At %d, %d in %d: %s\n", x, y, col, text.c_str());
+    const int line_height = large ? line_height_large : line_height_small;
+    const int pix_y = CONFIG_WIDTH - y * line_height;
+    const int erase_offset = 0;
+    if (erase)
+        lcdDrawFillRect(&dev, pix_y + erase_offset, 0, pix_y + erase_offset + line_height - 1, CONFIG_HEIGHT - 1, BLACK);
+    printf("At %d, %d in %d: %s\n", y, x, col, text.c_str());
     lcdDrawString(&dev,
                   large ? &fx_large : &fx_small,
-                  (max_y - y) * (large ? line_height_large : line_height_small),
+                  pix_y,
                   x * (large ? char_width_large : char_width_small),
                   reinterpret_cast<const uint8_t*>(text.c_str()), colours[col]);
     return true;
 }
 
-void touch()
+// x1,y1,x2,y2,col
+bool rect(const std::string s)
+{
+   std::vector<std::string> elems;
+   split(s, ",", elems);
+   if (elems.size() != 5)
+   {
+       printf("ERROR: Missing parameters\n");
+       return false;
+   }
+   int x1, y1, x2, y2, col;
+    if (!from_string(elems[0], x1) || x1 < 0 || x1 > CONFIG_HEIGHT)
+    {
+        printf("ERROR: Invalid X1\n");
+        return false;
+    }
+    if (!from_string(elems[1], y1) || y1 < 0 || y1 > CONFIG_WIDTH)
+    {
+        printf("ERROR: Invalid Y1\n");
+        return false;
+    }
+    if (!from_string(elems[2], x2) || x2 < 0 || x2 > CONFIG_HEIGHT)
+    {
+        printf("ERROR: Invalid X2\n");
+        return false;
+    }
+    if (!from_string(elems[3], y2) || y2 < 0 || y2 > CONFIG_WIDTH)
+    {
+        printf("ERROR: Invalid Y2\n");
+        return false;
+    }
+    if (!from_string(elems[4], col) || col < 0 || col > sizeof(colours)/sizeof(colours[0]))
+    {
+        printf("ERROR: Invalid colour\n");
+        return false;
+    }
+    printf("rect %d, %d, %d, %d, %d\n", y1, x1, y2, x2, col);
+    lcdDrawRect(&dev, y1, x1, y2, x2, colours[col]);
+    return true;
+}
+
+
+static int last_touch_x = 0;
+static int last_touch_y = 0;
+static TickType_t last_touch_tick = 0;
+
+void check_touch()
 {
     int level = gpio_get_level(XPT_IRQ);
-    if (level == 0)
+    if (level)
+        return;
+    last_touch_x = xptGetit(xpt_handle, XPT_START | XPT_XPOS | XPT_SER);
+    last_touch_y = xptGetit(xpt_handle, XPT_START | XPT_YPOS | XPT_SER);
+    last_touch_tick = xTaskGetTickCount();
+}
+
+void touch()
+{
+    if (xTaskGetTickCount() - last_touch_tick > 1000 / portTICK_RATE_MS)
     {
-        int xp = xptGetit(xpt_handle, (XPT_START | XPT_XPOS | XPT_SER));
-        int yp = xptGetit(xpt_handle, (XPT_START | XPT_YPOS | XPT_SER));
-        printf("T%d %d\n", xp, yp);
-    }
-    else
         printf("T\n");
+        return;
+    }
+    printf("T%d %d\n", last_touch_x, last_touch_y);
+}
+
+bool check_erase(std::string& s)
+{
+    if (s.empty() || (s[0] != 'e' && s[0] != 'E'))
+        return false;
+    s = s.substr(1);
+    return true;
 }
 
 bool idle = true;
@@ -214,6 +281,7 @@ void handle_line(const std::string& line)
     auto ch = line[0];
     auto rest = strip(line.substr(1));
     bool ok = true;
+    bool erase = false;
     switch (ch)
     {
     case 'c':
@@ -225,11 +293,18 @@ void handle_line(const std::string& line)
     case 'p':
         touch();
         break;
+    case 'r':
+        ok = rect(rest);
+        break;
     case 't':
-        text(false, rest);
+        // t[e][x],y,col,text
+        erase = check_erase(rest);
+        ok = text(false, erase, rest);
         break;
     case 'T':
-        text(true, rest);
+        // T[E][x],y,col,text
+        erase = check_erase(rest);
+        ok = text(true, erase, rest);
         break;
     case 'v':
     case 'V':
@@ -241,8 +316,6 @@ void handle_line(const std::string& line)
     }
     if (ok)
         printf("OK\n");
-    else
-        printf("ERROR\n");
 }
 
 extern "C" void console_task(void*)
@@ -258,6 +331,7 @@ extern "C" void console_task(void*)
     while (1)
     {
         vTaskDelay(10 / portTICK_RATE_MS);
+        check_touch();
 
         // Update idle animation if idle
         if (idle)
