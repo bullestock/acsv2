bc09ead3f96a0016ce885593b0dc5bd6b25dad38
Torsten Martinsen <torsten@bullestock.net>
2022-04-09 00:21:14 +0200

    Round.

--- frontend/case/bottom.py
+++ frontend/case/bottom.py
@@ -2,6 +2,7 @@ import cadquery as cq
 import cq_warehouse.extensions
 from cq_warehouse.fastener import HeatSetNut
 
+#!! TODO
 height = 100
 width = 120
 thickness = 25
@@ -9,9 +10,15 @@ thickness = 25
 holes_dx = 40.1109
 holes_dy = 42.11087
 
+#!! fix
 nut = HeatSetNut(size="M3-0.5-Standard", fastener_type="McMaster-Carr")
 
+# shell thickness
 th = 3
+# fillet radius
+fillet_r=thickness/2-2
+
+# standoff dimensions
 standoff_h = 5
 standoff_d = 7
 
@@ -30,6 +37,7 @@ result = (cq.Workplane("XY")
           # add insert holes to standoffs
           .vertices(tag="screwholes")
           .insertHole(fastener=nut)
+          # round edges
+          .edges("<Z or |Z").fillet(fillet_r)
           )
 
-# dist  42.11087, 40.1109
