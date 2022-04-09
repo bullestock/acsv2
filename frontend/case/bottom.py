f42a9f80dd61f77a75f29266503d9d911e5479fc
Torsten Martinsen <torsten@bullestock.net>
2022-04-09 17:32:40 +0200

    Basic box.

--- frontend/case/bottom.py
+++ frontend/case/bottom.py
@@ -1,6 +1,5 @@
 import cadquery as cq
 import cq_warehouse.extensions
-from cq_warehouse.fastener import HeatSetNut
 
 #!! TODO
 height = 100
@@ -10,8 +9,9 @@ thickness = 25
 holes_dx = 40.1109
 holes_dy = 42.11087
 
-#!! fix
-nut = HeatSetNut(size="M3-0.5-Standard", fastener_type="McMaster-Carr")
+# M3x4x4
+insert_l = 4
+insert_r = 2.1
 
 # shell thickness
 th = 3
@@ -22,22 +22,42 @@ fillet_r=thickness/2-2
 standoff_h = 5
 standoff_d = 7
 
-result = (cq.Workplane("XY")
+# define standoff
+standoff = (cq.Workplane()
+            .cylinder(radius=standoff_d/2, height=standoff_h)
+            .faces(">Z")
+            .circle(insert_r).cutBlind(-insert_l)
+            )
+
+# make shell
+shell = (cq.Workplane()
           .box(width, height, thickness)
           .faces(">Z")
           .shell(-th)
-          .faces("<Z")
+          # round edges
+          .edges("<Z or |Z").fillet(fillet_r)
+          )
+
+# distribute standoffs
+standoffs = (shell
           .workplane(origin=(0, 0, 0)) # place OPi
           # place standoffs on bottom
           .transformed(offset=(0, 0, -th))
           .rect(holes_dx, holes_dy, forConstruction=True)
           .vertices()
-          .tag("screwholes")
-          .cylinder(radius=standoff_d/2, height=standoff_h)
-          # add insert holes to standoffs
-          .vertices(tag="screwholes")
-          .insertHole(fastener=nut)
-          # round edges
-          .edges("<Z or |Z").fillet(fillet_r)
+          .eachpoint(lambda loc: standoff.val().moved(loc), True)
           )
+
+# combine
+result = shell.union(standoffs)
+
+show_object(result)
+
+# TODO:
+# screw holes for wall fitting
+# cutouts for USB
+# cutout + round hole for ethernet cable
+# stepdown
+# power plug
+# 12 V out for lock
 
