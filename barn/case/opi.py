1819af9d436363c0b613b3713cc9f9ea4c8716be
Torsten Martinsen <torsten@bullestock.net>
2022-04-10 11:12:19 +0200

    Construct piecewise.

--- barn/case/opi.py
+++ barn/case/opi.py
@@ -16,16 +16,20 @@ disp_h = 4
 centerXY = (True, True, False)
 
 def opi(obj):
-    return (obj
-            .transformed(offset=(0, 0, 0))
-            .box(opi_l, opi_w, opi_h, centered=centerXY)
-            .faces(">Y")
-            .workplane(origin=(0, 0, 0))
-            .transformed(offset=(-eth_w/2-eth_x_offset, pcb_th, 0))
-            .box(eth_w, 13.5, 5, centered=False)
-            .faces(">Z")
-            .workplane(origin=(0, 0, 0))
-            .transformed(offset=(0, -35, 0))
-            .box(disp_l, disp_w, disp_h, centered=centerXY)
-            )
+    pi = (obj
+          .transformed(offset=(0, 0, 0))
+          .box(opi_l, opi_w, opi_h, centered=centerXY))
+    eth = (pi
+           .faces(">Y")
+           .workplane()
+           .transformed(offset=(-eth_w/2-eth_x_offset, pcb_th, 0))
+           .box(eth_w, 13.5, 5, centered=False)
+           )
+    display = (pi
+               .faces(">Z")
+               .workplane()
+               .transformed(offset=(0, -35, 0))
+               .box(disp_l, disp_w, disp_h, centered=centerXY)
+               )
+    return pi.union(eth).union(display)
 
