6b59fc21aba0bb6a2a977293573275e00c35a5bd
Torsten Martinsen <torsten@bullestock.net>
2022-04-11 21:22:57 +0200

    Flat version.

--- barn/case/dispsupport.py
+++ barn/case/dispsupport.py
@@ -6,43 +6,25 @@ foot_hole_d = 3.5
 disp_hole_d = 2.5
 dhx_cc = 0.91*25.4
 dhy_cc = 0.93*25.4
-width = 10
+width = 12
 th = 3
-
-foot = (cq.Workplane("XY")
-       .box(width, width, th, centered=(True, True, False))
-       .circle(foot_hole_d/2)
-       .cutThruAll()
-       )
 
 result = (cq.Workplane("XY")
           .tag("base")
+          .box(mh_cc+width, width, th, centered=(True, True, False))
           .rarray(mh_cc, 1, 2, 1)
-          .eachpoint(lambda loc: foot.val().moved(loc), True)
+          .slot2D(foot_hole_d*2, foot_hole_d, 90)
+          .cutThruAll()
          )
-bw = 40
 offset = 2
 
 result = (result
-          .rarray(bw, 1, 2, 1)
-          .rect(th, width)
-          .extrude(total_h-offset)
-          )
-
-result = (result
           .workplaneFromTagged("base")
-          .transformed(offset=(0, 0, total_h-th-offset))
-          .box(bw, width, th, centered=(True, True, False))
-          )
-
-result = (result
-          .workplaneFromTagged("base")
-          .transformed(offset=(0, 0, total_h-th-offset))
           .box(30, 30, th, centered=(True, True, False))
           .edges("|Z")
           .fillet(1)
           .workplaneFromTagged("base")
-          .transformed(offset=(0, 0, total_h-th+offset/2))
+          .transformed(offset=(0, 0, th))
           .rect(dhx_cc , dhx_cc, forConstruction=True)
           .vertices()
           .circle(disp_hole_d)
