426f2974287db4f95b0d69b5e169d77f53c680f7
Torsten Martinsen <torsten@bullestock.net>
2022-04-11 09:19:09 +0200

    Allow room for screws.

--- barn/case/standoffs.py
+++ barn/case/standoffs.py
@@ -1,14 +1,19 @@
 import cadquery as cq
+import math
 
 # M3x4x4
 insert_l = 4
 insert_r = 2.1
+insert_sr = 1.5
 
 def round_standoff(d, h):
+    max_d = min(h, 3*insert_l)
     return (cq.Workplane()
             .cylinder(radius=d/2, height=h)
             .faces(">Z")
             .circle(insert_r).cutBlind(-insert_l)
+            .faces(">Z")
+            .circle(insert_sr+.25).cutBlind(-max_d)
             )
 
 def square_standoff(d, h, r):
@@ -17,4 +22,6 @@ def square_standoff(d, h, r):
             .edges("|Z").fillet(r)
             .faces(">Z")
             .circle(insert_r).cutBlind(-insert_l)
+            .faces(">Z")
+            .circle(insert_sr+.25).cutBlind(-3*insert_l)
             )
