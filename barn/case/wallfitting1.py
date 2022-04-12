9c878462bfbbf535a9313ce253b7d9716967532a
Torsten Martinsen <torsten@bullestock.net>
2022-04-12 12:09:54 +0200

    Fix bugs.

--- barn/case/wallfitting1.py
+++ barn/case/wallfitting1.py
@@ -8,13 +8,14 @@ th1 = 3
 # thickness of trapezoidal part
 th2 = 5
 # minimum width of trapezoidal part
-min_w = 20
+min_w = 30
+slope = 0.7
 
 result = (cq.Workplane("XY")
-          .hLine(-w/2)
-          .vLine(th1)
-          .hLine(w/2 - min_w/2)
-          .lineTo(-(w/2 - min_w/2 - th2), th1+th2)
+          .hLine(-w/2) # bottom
+          .vLine(th1)  # edge
+          .hLine(w/2 - min_w/2) # flat upper part
+          .lineTo(-(min_w/2 + slope*th2), th1+th2)
           .lineTo(0, th1+th2)
           .mirrorY()
           .extrude(h)
