e9caf60a9d132ca8253cabfc108ef61eeba140eb
Torsten Martinsen <torsten@bullestock.net>
2022-04-18 10:15:47 +0200

    Fix crash.

--- backend/acs/context_processors.py
+++ backend/acs/context_processors.py
@@ -1,4 +1,4 @@
 from django.conf import settings
 
 def selected_settings(request):
-    return {'APP_VERSION_NUMBER': settings.APP_VERSION_NUMBER, 'GIT_COMMIT': settings.GIT_COMMIT}
+    return {'GIT_COMMIT': settings.GIT_COMMIT,}
