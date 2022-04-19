09f73b122575f7cbfb0027c03dfe9e3b96471f9d
Torsten Martinsen <torsten@bullestock.net>
2022-04-19 17:15:30 +0200

    Add alternative cut method.

--- frontend/case/orangepizero.py
+++ frontend/case/orangepizero.py
@@ -8,7 +8,7 @@ opi_h = 22
 eth_w = 20.8
 eth_h = 17.5
 # usb
-usb_w = 17
+usb_w = 18
 usb_h = 10
 usb_z = 18
 usb_x_cc = 20
@@ -35,4 +35,20 @@ def opi_jacks_cut(obj, x_offset, y_offset, z_offset):
                          rotate=(90, 90, 0))
             .rect(eth_w, eth_h, centered=(True, False))
             .cutBlind(100)
+            )
+
+def opi_jacks_cutter(x_offset, y_offset, z_offset):
+    return (cq.Workplane("XY")
+            .tag("base")
+            .transformed(offset=(x_offset, y_offset+usb_x_offset, z_offset+usb_z),
+                         rotate=(90, 90, 0))
+            .rarray(usb_x_cc, 1, 2, 1)
+            .rect(usb_w, usb_h, centered=True)
+            .extrude(-50)
+            .edges(">Z or <Z").fillet(2)
+            .workplaneFromTagged("base")
+            .transformed(offset=(x_offset, y_offset+usb_x_offset, z_offset),
+                         rotate=(90, 90, 0))
+            .rect(eth_w, eth_h, centered=(True, False))
+            .extrude(100)
             )
