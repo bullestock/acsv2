f51d4e6556295a4c3c7fafae7860f75cf9e00c53
Torsten Martinsen <torsten@bullestock.net>
2022-04-29 21:36:38 +0200

    Fix copy-paste bug.

--- barn/case/dispsupport.py
+++ barn/case/dispsupport.py
@@ -1,4 +1,5 @@
 import cadquery as cq
+from defs import *
 
 mh_cc = 50
 total_h = 31+2.6
@@ -8,15 +9,13 @@ dhx_cc = 30.48
 dhy_cc = 28.448
 width = 12
 th = 3
-# display is shifted this much to the right
-offset = -0.625
 
 result = (cq.Workplane("XY")
           .tag("base")
           # bar
           .box(mh_cc+width, width, th, centered=(True, True, False))
           # holes for case screws
-          .transformed(offset=(offset, 0, 0))
+          .transformed(offset=(disp_x_offset, 0, 0))
           .rarray(mh_cc, 1, 2, 1)
           .slot2D(foot_hole_d*2, foot_hole_d, 90)
           .cutThruAll()
@@ -26,18 +25,18 @@ offset = 2
 result = (result
           # display mount plate
           .workplaneFromTagged("base")
-          .box(36, 36, th, centered=(True, True, False))
+          .box(36, 34, th, centered=(True, True, False))
           .edges("|Z")
           .fillet(1)
           # supports for display
           .workplaneFromTagged("base")
           .transformed(offset=(0, 0, th))
-          .rect(dhx_cc, dhx_cc, forConstruction=True)
+          .rect(dhx_cc, dhy_cc, forConstruction=True)
           .vertices()
           .circle(disp_hole_d)
           .extrude(2)
           # holes for display screws
-          .rect(dhx_cc, dhx_cc, forConstruction=True)
+          .rect(dhx_cc, dhy_cc, forConstruction=True)
           .vertices()
           .circle(disp_hole_d/2)
           .cutThruAll()
