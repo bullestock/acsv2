b8cece7af3b101e11d9aec43d7645f376d714bf8
Torsten Martinsen <torsten@bullestock.net>
2022-04-29 20:47:52 +0200

    Update for correct display.

--- barn/case/dispsupport.py
+++ barn/case/dispsupport.py
@@ -4,16 +4,19 @@ mh_cc = 50
 total_h = 31+2.6
 foot_hole_d = 3.5
 disp_hole_d = 2.5
-dhx_cc = 0.91*25.4
-dhy_cc = 0.93*25.4
+dhx_cc = 30.48
+dhy_cc = 28.448
 width = 12
 th = 3
+# display is shifted this much to the right
+offset = -0.625
 
 result = (cq.Workplane("XY")
           .tag("base")
           # bar
           .box(mh_cc+width, width, th, centered=(True, True, False))
           # holes for case screws
+          .transformed(offset=(offset, 0, 0))
           .rarray(mh_cc, 1, 2, 1)
           .slot2D(foot_hole_d*2, foot_hole_d, 90)
           .cutThruAll()
@@ -23,7 +26,7 @@ offset = 2
 result = (result
           # display mount plate
           .workplaneFromTagged("base")
-          .box(30, 30, th, centered=(True, True, False))
+          .box(36, 36, th, centered=(True, True, False))
           .edges("|Z")
           .fillet(1)
           # supports for display
