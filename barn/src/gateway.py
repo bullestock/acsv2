f72c5c6d071b7589e716f9d53c84f75bedf7f32c
Torsten Martinsen <torsten@bullestock.net>
2022-05-14 11:29:32 +0200

    Add gateway interface.

---
+++ barn/src/gateway.py
@@ -0,0 +1,18 @@
+import requests, json, os
+
+class Gateway:
+
+    URL = 'https://acsgateway.hal9k.dk/acsheartbeat'
+    TOKEN = os.environ['GW_TOKEN']
+    
+    def ping(self):
+        data = {}
+        data['token'] = self.TOKEN
+        response = requests.post(self.URL, data=json.dumps(data),
+                                 headers={"Content-Type": "application/json", "Accept": "application/json"})
+        if response.status_code != 200:
+            print("Bad gateway response: %s" % response)
+        
+if __name__ == "__main__":
+    gw = Gateway()
+    gw.ping()
