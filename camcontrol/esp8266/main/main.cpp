f3f509fea53ecd034f05bb42559053770a9217a4
Torsten Martinsen <torsten@bullestock.net>
2022-05-19 20:34:13 +0200

    Implement manual relay control.

--- camcontrol/esp8266/main/main.cpp
+++ camcontrol/esp8266/main/main.cpp
@@ -18,21 +18,22 @@ extern "C"
 void app_main()
 {
     init_gpio();
-    the_led.set_color(RgbColor(0, 0, 128));
+    Led the_led;
     connect();
     the_led.set_color(RgbColor(0, 255, 0));
-    
+
+    bool relay_on = false;
     while (1)
     {
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        printf("on\n");
-        the_led.set_color(RgbColor(255, 0, 0));
-        set_relay(1);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        printf("off\n");
-        the_led.set_color(RgbColor(0, 255, 128));
-        set_relay(0);
+        vTaskDelay(10 / portTICK_RATE_MS);
         const auto buttons = read_buttons();
-        printf("R %d G %d\n", buttons.first, buttons.second);
+        const auto old_relay_on = relay_on;
+        if (buttons.first)
+            relay_on = false;
+        else if (buttons.second)
+            relay_on = true;
+        if (relay_on != old_relay_on)
+            the_led.set_color(relay_on ? RgbColor(255, 0, 0) : RgbColor(0, 255, 0));
+        set_relay(relay_on);
     }
 }
