925c0c18cfb0ce0babf9be4fba0ec9d9198448a2
Torsten Martinsen <torsten@bullestock.net>
2022-05-20 16:14:07 +0200

    Fix connection.

--- camcontrol/esp8266/main/connect.cpp
+++ camcontrol/esp8266/main/connect.cpp
@@ -1,125 +1,138 @@
-/* WiFi station Example
+/* Common functions for protocol examples, to establish Wi-Fi or Ethernet connection.
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
-*/
+ */
+
 #include <string.h>
+
+#include "protocol_examples_common.h"
+#include "sdkconfig.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_event_loop.h"
+#include "tcpip_adapter.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-#include "esp_system.h"
-#include "esp_log.h"
-#include "esp_netif.h"
-#include "esp_event.h"
-#include "esp_wifi.h"
-#include "nvs.h"
 #include "nvs_flash.h"
-
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#define EXAMPLE_ESP_WIFI_SSID      "bullestock-guest"
-#define EXAMPLE_ESP_WIFI_PASS      ""
-#define EXAMPLE_ESP_MAXIMUM_RETRY  3
+#define CONFIG_EXAMPLE_WIFI_SSID      "bullestock-guest"
+#define CONFIG_EXAMPLE_WIFI_PASSWORD  ""
+#define CONFIG_EXAMPLE_MAXIMUM_RETRY  3
 
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
+#define GOT_IPV4_BIT BIT(0)
+#define GOT_IPV6_BIT BIT(1)
 
-/* The event group allows multiple bits for each event, but we only care about two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+#define CONNECTED_BITS (GOT_IPV4_BIT)
 
-static const char *TAG = "CAMCTL";
+static EventGroupHandle_t s_connect_event_group;
+static ip4_addr_t s_ip_addr;
+static char s_connection_name[32] = CONFIG_EXAMPLE_WIFI_SSID;
+static char s_connection_passwd[32] = CONFIG_EXAMPLE_WIFI_PASSWORD;
 
-static int s_retry_num = 0;
+static const char* TAG = "CAMCTL";
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
+static void on_wifi_disconnect(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data)
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
-    {
-        ESP_LOGI(TAG, "connect");
-        esp_wifi_connect();
-        return;
-    }
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
-    {
-        ESP_LOGI(TAG, "retry: %d", s_retry_num);
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        }
-        else
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        ESP_LOGI(TAG,"connect to the AP fail");
-        return;
-    }
-    if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
-    {
-        auto event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:%s", ip4addr_ntoa(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-        return;
-    }
+    auto event = (system_event_sta_disconnected_t*) event_data;
+
+    ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
+    if (event->reason == WIFI_REASON_BASIC_RATE_NOT_SUPPORT)
+        /*Switch to 802.11 bgn mode */
+        esp_wifi_set_protocol(ESP_IF_WIFI_STA, WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G | WIFI_PROTOCOL_11N);
+    ESP_ERROR_CHECK(esp_wifi_connect());
 }
 
-void connect()
+static void on_got_ip(void* arg, esp_event_base_t event_base,
+                      int32_t event_id, void* event_data)
 {
-    s_wifi_event_group = xEventGroupCreate();
+    auto event = (ip_event_got_ip_t*) event_data;
+    memcpy(&s_ip_addr, &event->ip_info.ip, sizeof(s_ip_addr));
+    xEventGroupSetBits(s_connect_event_group, GOT_IPV4_BIT);
+}
 
-    tcpip_adapter_init();
-
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
+static void start()
+{
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip, NULL));
 
-    wifi_config_t wifi_config;
-    strcpy((char*) wifi_config.sta.ssid, EXAMPLE_ESP_WIFI_SSID);
-    strcpy((char*) wifi_config.sta.password, EXAMPLE_ESP_WIFI_PASS);
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_config_t wifi_config = { 0 };
 
-    /* Setting a password implies station will connect to all security modes including WEP/WPA.
-        * However these modes are deprecated and not advisable to be used. Incase your Access point
-        * doesn't support WPA2, these mode can be enabled by commenting below line */
+    strncpy((char*) &wifi_config.sta.ssid, s_connection_name, 32);
+    strncpy((char*) &wifi_config.sta.password, s_connection_passwd, 32);
 
-    if (strlen((char *)wifi_config.sta.password)) {
-        wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
-    }
-
+    ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
+}
 
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
+static void stop()
+{
+    esp_err_t err = esp_wifi_stop();
+    if (err == ESP_ERR_WIFI_NOT_INIT)
+        return;
+    ESP_ERROR_CHECK(err);
 
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-                                           pdFALSE,
-                                           pdFALSE,
-                                           portMAX_DELAY);
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip));
 
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
-    if (bits & WIFI_CONNECTED_BIT)
-        ESP_LOGI(TAG, "connected to ap");
-    else if (bits & WIFI_FAIL_BIT)
-        ESP_LOGI(TAG, "Failed to connect to %s", EXAMPLE_ESP_WIFI_SSID);
-    else
-        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+}
 
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
-    vEventGroupDelete(s_wifi_event_group);
+esp_err_t connect()
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    
+    if (s_connect_event_group)
+        return ESP_ERR_INVALID_STATE;
+
+    s_connect_event_group = xEventGroupCreate();
+    start();
+    xEventGroupWaitBits(s_connect_event_group, CONNECTED_BITS, true, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "Connected to %s", s_connection_name);
+    ESP_LOGI(TAG, "IPv4 address: " IPSTR, IP2STR(&s_ip_addr));
+    return ESP_OK;
+}
+
+esp_err_t disconnect()
+{
+    if (!s_connect_event_group)
+        return ESP_ERR_INVALID_STATE;
+
+    vEventGroupDelete(s_connect_event_group);
+    s_connect_event_group = NULL;
+    stop();
+    ESP_LOGI(TAG, "Disconnected from %s", s_connection_name);
+    s_connection_name[0] = '\0';
+    return ESP_OK;
+}
+
+esp_err_t example_set_connection_info(const char* ssid, const char* passwd)
+{
+    strncpy(s_connection_name, ssid, sizeof(s_connection_name));
+    strncpy(s_connection_passwd, passwd, sizeof(s_connection_passwd));
+
+    return ESP_OK;
 }
