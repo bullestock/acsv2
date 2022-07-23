1bfc77016463338b0c36f2d187637e0ed9f489dd
Torsten Martinsen <torsten@bullestock.net>
2022-07-23 20:56:43 +0200

    Python 3.

--- ui/fixports.sh
+++ ui/fixports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python idf_monitor.py -p /dev/ttyUSB0 -b 115200
-python idf_monitor.py -p /dev/ttyUSB1 -b 115200
-python idf_monitor.py -p /dev/ttyUSB2 -b 115200
+python3 idf_monitor.py -p /dev/ttyUSB0 -b 115200
+python3 idf_monitor.py -p /dev/ttyUSB1 -b 115200
+python3 idf_monitor.py -p /dev/ttyUSB2 -b 115200
