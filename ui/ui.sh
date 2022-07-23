8dd568acb4c7f91bb1561089df222ec776674761
Torsten Martinsen <torsten@bullestock.net>
2022-07-23 21:01:50 +0200

    Fix dir.

--- ui/ui.sh
+++ ui/ui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ruby /home/torsten/acsv2/ui/init.rb | tee /home/torsten/acsv2/ui.log
-ruby /home/torsten/acsv2/ui/ui.rb | tee -a /home/torsten/acsv2/uiv2.log
+ruby /home/torsten/acsv2/ui/init.rb | tee /home/torsten/acsv2/ui/ui.log
+ruby /home/torsten/acsv2/ui/ui.rb | tee -a /home/torsten/acsv2/ui/ui.log
