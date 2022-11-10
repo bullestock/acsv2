efbda5b5b7d87049744a5dc09fcd49512a6bd21b
Torsten Martinsen <torsten@bullestock.net>
2022-11-10 17:51:30 +0100

    Fix errors.

--- barn/src/slack.py
+++ barn/src/slack.py
@@ -18,7 +18,7 @@ class Slack:
         return self.last_status
   
     def send_message(self, msg):
-        gw.log("%s SLACK: %s" % (datetime.now(), msg))
+        self.gw.log("%s SLACK: %s" % (datetime.now(), msg))
         try:
             body = { 'channel': "monitoring", 'icon_emoji': ":panopticon:", 'parse': "full", "text": msg }
             headers = {
@@ -26,9 +26,9 @@ class Slack:
                 "Authorization": "Bearer %s" % self.token
             }
             r = requests.post(url = "https://slack.com/api/chat.postMessage", data = body, headers = headers)
-            gw.log("%s Slack retcode: %d" % (datetime.now, r.status_code))
+            self.gw.log("%s Slack retcode: %d" % (datetime.now, r.status_code))
         except Exception as e:
-            gw.log("%s Slack exception: %s" % (datetime.now, e))
+            self.gw.log("%s Slack exception: %s" % (datetime.now, e))
 
 if __name__ == "__main__":
     s = Slack()
