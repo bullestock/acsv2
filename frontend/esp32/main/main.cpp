e5c288a2b84f63091207d4d8afcf541ee59aa372
Torsten Martinsen <torsten@bullestock.net>
2023-08-20 15:55:29 +0200

    Add WiFi code.

--- frontend/esp32/main/main.cpp
+++ frontend/esp32/main/main.cpp
@@ -4,10 +4,15 @@
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 
+#include "connect.h"
 #include "console.h"
 #include "defs.h"
+#include "format.h"
 #include "hw.h"
+#include "gateway.h"
 #include "rs485.h"
 
 #include <TFT_eSPI.h>
@@ -34,6 +39,9 @@ void set_status(TFT_eSPI& tft, const std::string& status, uint16_t colour = TFT_
     const auto x = TFT_HEIGHT/2 + TFT_HEIGHT/4 - w/2;
     const auto y = TFT_HEIGHT/2 - 20;
     tft.drawString(status.c_str(), x, y, GFXFF);
+
+    //!!
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
 
 void fatal_error(TFT_eSPI& tft, const std::string& error)
@@ -54,8 +62,16 @@ void app_main()
 {
     init_hardware();
     init_rs485();
-    
+
     printf("ACS frontend v %s\n", VERSION);
+
+    TFT_eSPI tft;
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK);
+    tft.setFreeFont(small_font);
+    tft.setTextColor(TFT_CYAN);
+    set_status(tft, format("ACS v %d", VERSION));
 
     printf("\n\nPress a key to enter console\n");
     bool debug = false;
@@ -71,16 +87,30 @@ void app_main()
     if (debug)
         run_console();        // never returns
 
-    TFT_eSPI tft;
-    tft.init();
-    tft.setRotation(1);
-    tft.fillScreen(TFT_BLACK);
-    tft.setFreeFont(small_font);
-    tft.setTextColor(TFT_CYAN);
-    tft.drawString("ACS", TFT_HEIGHT/2, TFT_WIDTH/2, GFXFF);
+    set_status(tft, "NVS init");
     
     printf("\nStarting application\n");
 
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    set_status(tft, "Connect to WiFi");
+
+    ESP_ERROR_CHECK(connect({ "hal9k", "bullestock" }));
+    ESP_LOGI(TAG, "Connected to WiFi");
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+
+    set_status(tft, "Connected");
+    
+    xTaskCreate(gw_task, "gw_task", 4*1024, NULL, 1, NULL);
+    
     uint64_t last_tick = 0;
     while (1)
     {
