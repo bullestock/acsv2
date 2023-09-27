af121adee641bccbfe2518ec5c26826e275b1e61
Torsten Martinsen <torsten@bullestock.net>
2023-09-27 18:12:31 +0200

    Add bottom

---
+++ reader/box/door_defs.py
@@ -0,0 +1,25 @@
+case_th = 3
+case_h = 40
+case_d = 18
+case_w = 180
+coil_sup_l = 5
+coil_sup_d = 3
+# Inner diameter of coil
+coil_sup_w = 43
+coil_sup_h = 31
+# Width of screw block
+sw = 12
+# Thickness of front plate
+front_th = 4
+# Screw cutout
+sc_w = 10
+sc_d = 5
+# Screw holes
+sh_dia = 4.2
+sh_dist = 169.5 # wack
+# LED holes
+led_dia = 5.1
+led_dist = 17.17
+led_offset = case_w/2 - sw - 7.5
+iw = case_w - 2*sw
+ih = case_h - case_th
