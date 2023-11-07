54306ac55e39d025d943defeb2371835ec678162
Torsten Martinsen <torsten@bullestock.net>
2023-11-07 20:05:53 +0100

    Add --preload to Gunicorn command to avoid multiple instances of scheduler.

--- backend/machines/apps.py
+++ backend/machines/apps.py
@@ -1,9 +1,13 @@
 from django.apps import AppConfig
 
+import logging
+
 class MachinesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'machines'
 
     def ready(self):
+        logger = logging.getLogger("django")
+        logger.info('Starting scheduler')
         from synch import updater
         updater.start()
