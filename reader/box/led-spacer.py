cddc5b4a99c954c5cc69c7ec24679ebf448f9d2f
Torsten Martinsen <torsten@bullestock.net>
2023-11-18 23:47:44 +0100

    Spacer for LED.

---
+++ reader/box/led-spacer.py
@@ -0,0 +1,16 @@
+import cadquery as cq
+
+from door_defs import *
+
+res = (cq.Workplane("XY")
+       .tag("base")
+       .circle(5/2)
+       .extrude(5)
+       .workplaneFromTagged("base")
+       .rarray(1, 2.54, 1, 2)
+       .circle(1/2)
+       .cutThruAll()
+       )
+
+show_object(res)
+
