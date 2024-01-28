036575659dad732d0058db9922020103749c9e63
Torsten Martinsen <torsten@bullestock.net>
2024-01-28 21:27:59 +0100

    Bump.

--- frontend/esp32/main/defs.h
+++ frontend/esp32/main/defs.h
@@ -5,7 +5,7 @@
 
 #include <driver/gpio.h>
 
-constexpr const char* VERSION = "1.3.4";
+constexpr const char* VERSION = "1.3.5";
 
 constexpr const char* IDENTIFIER_KEY = "id";
 constexpr const char* WIFI_KEY = "wifi";
